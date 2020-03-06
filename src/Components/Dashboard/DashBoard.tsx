4005a8abec503d13241344dac8fc01c3a207c6b2
amahjennifer <jennifer000@rocketmail>
2020-03-06 19:12:17 +0100

    Add deposit transactions

--- src/Components/Dashboard/DashBoard.tsx
+++ src/Components/Dashboard/DashBoard.tsx
@@ -1,15 +1,15 @@
 import React, { useContext, useEffect } from "react";
 import { useAuthContext } from "../../context/auth/authContext";
 import { withRouter } from "react-router";
-import "./dashboard.css"
-import transferImg from "../../transfer.svg"
-import MyVerticallyCenteredModal from "../Modal/Modal"
-import WModal from "../Modal/WModal"
+import "./dashboard.css";
+import transferImg from "../../transfer.svg";
+import MyVerticallyCenteredModal from "../Modal/Modal";
+import WithdrawModal from "../Modal/WModal";
 const DashBoard = () => {
 	// const authContext = useAuthContext()
 	const {
 		loadUser,
-		state: { user }
+		state: { user, userTransactions }
 	} = useAuthContext();
 
 	useEffect(() => {
@@ -18,17 +18,23 @@ const DashBoard = () => {
 	}, []);
 	const [modalShow, setModalShow] = React.useState(false);
 	const [wmodalShow, setWModalShow] = React.useState(false);
+	// const tableList = userTransactions.map((transaction) => {
+	// 	return (
+	// 		<>
+	// 				<tr>{transaction.amount}</tr>
+	// 			<tr>{transaction.balance}</tr>
+	// 			<tr>{transaction.transactionType}</tr>
+	// 			</>
+	// 	);
+
+	// })
 	return (
 		<div className="new-container">
-			{/* <div className="sideButton">
-				<img src={transferImg} />
-				<img src={transferImg} />
-			</div> */}
 			<div className="main bordered">
 				<div className="cash">
 					<div className="cashText">
 						<h2>Cash</h2>
-						<p>{user?.balance}</p>
+						<p>{user ? user.balance: 'loading...'}</p>
 					</div>
 					<div className="buttons">
 						<button
@@ -37,11 +43,15 @@ const DashBoard = () => {
 						>
 							Deposit
 						</button>
+						<WithdrawModal show={wmodalShow} onHide={() => setWModalShow(false)} />
 						<MyVerticallyCenteredModal
 							show={modalShow}
 							onHide={() => setModalShow(false)}
 						/>
-						<WModal show={wmodalShow} onHide={() => setWModalShow(false)} />
+						{/* <WithdrawModal
+							show={wmodalShow}
+							onHide={() => setWModalShow(false)}
+						/> */}
 						<button
 							className="withdrawButton"
 							onClick={() => setWModalShow(true)}
@@ -49,61 +59,27 @@ const DashBoard = () => {
 							Withdraw
 						</button>
 					</div>
-					{/* </div> */}
 				</div>
 				<div className="transaction">
-					<table className="transaction-table">
-						<tr>
+					<table className="transaction-table ">
+						<tr className="tableHead">
 							<th>Date</th>
 							<th>Amount</th>
 							<th>Balance</th>
 						</tr>
-						<tr>
-							<td>Alfreds Futterkiste</td>
-							<td>Maria Anders</td>
-							<td>Germany</td>
-						</tr>
+
+						{userTransactions.map(transaction => (
+							<tr>
+								<td>{transaction.balance}</td>
+								<td>{transaction.transactionType}</td>
+								<td>{transaction.amount}</td>
+							</tr>
+						))}
 					</table>
 				</div>
-
-				{/* <div className="transactionList">
-					<p>2002-03-12</p>
-					<p>100000</p>
-					<p>200001556</p>
-				</div> */}
 			</div>
 		</div>
 	);
 };
 
-//modal function 
-// const ModalBasicExample = () => (
-// 	<Modal trigger={<Button>Deposit</Button>} basic size="small">
-// 		<Header icon="archive" content="Archive Old Messages" />
-// 		<Modal.Content>
-// 			<Form>
-// 				<Form.Field>
-// 					<label>First Name</label>
-// 					<input placeholder="First Name" />
-// 				</Form.Field>
-// 				<Form.Field>
-// 					<label>Last Name</label>
-// 					<input placeholder="Last Name" />
-// 				</Form.Field>
-// 				<Form.Field>
-// 					<Checkbox label="I agree to the Terms and Conditions" />
-// 				</Form.Field>
-// 				<Button type="submit">Submit</Button>
-// 			</Form>
-// 		</Modal.Content>
-// 		<Modal.Actions>
-// 			<Button basic color="red" inverted>
-// 				<Icon name="remove" /> No
-// 			</Button>
-// 			<Button color="green" inverted>
-// 				<Icon name="checkmark" /> Yes
-// 			</Button>
-// 		</Modal.Actions>
-// 	</Modal>
-// );
 export default withRouter(DashBoard);
--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useContext, ChangeEvent, FormEvent } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import AlertContext from "../../alert/alertContext"
+import {useAuthContext} from "../../context/auth/authContext"
 
-function MyVerticallyCenteredModal(props:any) {
+function MyVerticallyCenteredModal(props: any) {
+	const { depositFunc } = useAuthContext();
+	const { setAlert } = useContext(AlertContext);
+	const [deposit, setDeposit] = useState({
+		type: "Credit",
+		amount: 0
+	});
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+		setDeposit({ ...deposit, [e.target.name]:Number( e.target.value )});
+
+	const { amount,type } = deposit;
+	
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		//check if input COntains numbers 
+		
+		if (amount === 0) {
+			setAlert("Please Enter Amount ", "danger");
+			return;
+		}
+	
+
+		depositFunc({ type, amount })
+	}
 	return (
 		<Modal
 			{...props}
@@ -17,24 +42,36 @@ function MyVerticallyCenteredModal(props:any) {
 					<b>Deposit</b>
 				</Modal.Title>
 			</Modal.Header>
+			<form onSubmit={handleSubmit}>
 			<Modal.Body>
-				<InputGroup size="lg">
+				
+						<InputGroup size="lg">
 					<InputGroup.Prepend>
 						<InputGroup.Text id="inputGroup-sizing-lg">
 							Enter Amount{" "}
 						</InputGroup.Text>
 					</InputGroup.Prepend>
 					<FormControl
-						aria-label="Large"
+							aria-label="Large"
+							name="amount"
 						aria-describedby="inputGroup-sizing-sm"
-						placeholder="100000000000"
+							placeholder="100000000000"
+							type="number"
+						onChange={handleChange}
 					/>
 				</InputGroup>
+			
+			
 			</Modal.Body>
+				
 			<Modal.Footer>
-				<Button variant="primary">Deposit</Button>
-				<Button onClick={props.onHide} variant="danger">Cancel</Button>
-			</Modal.Footer>
+				<Button type="submit" variant="primary" >Deposit</Button>
+				<Button onClick={props.onHide} variant="danger">
+					Cancel
+				</Button>
+				</Modal.Footer>
+						</form>
+			
 		</Modal>
 	);
 }
